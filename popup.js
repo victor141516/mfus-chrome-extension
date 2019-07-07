f3d489b8cd2f96a00cf0af0ecfccafc74546c12c
victor141516 <yomellamovictor@gmail.com>
2019-07-07 19:02:26 +0200

    Initial commit

---
+++ popup.js
@@ -0,0 +1,33 @@
+const basePath = 'https://mfus.tk';
+
+chrome.tabs.query({
+  'active': true
+}, tabs => {
+  const currentUrl = tabs.pop().url;
+  fetch(`${basePath}/short`, {
+      'method': 'POST',
+      'mode': 'cors',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        longUrl: currentUrl
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      const shortUrl = `${basePath}/${res.short}`
+      const codeInput = document.getElementById('code');
+      document.getElementById('result').classList.toggle('hidden');
+      document.getElementById('spinner').classList.toggle('hidden');
+
+      codeInput.value = shortUrl;
+      codeInput.focus();
+      codeInput.onkeypress = key => {
+        if (key.charCode === 13) {
+          codeInput.select();
+          document.execCommand('copy');
+        }
+      }
+    });
+});
